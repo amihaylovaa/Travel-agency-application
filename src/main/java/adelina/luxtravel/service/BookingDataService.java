da6bd254852a09469830632094a5011b3ff48492
amihaylovaa <--->
2019-12-01 20:29:50 +0200

    add booking serivce

--- src/main/java/adelina/luxtravel/service/BookingDataService.java
+++ src/main/java/adelina/luxtravel/service/BookingDataService.java
@@ -1,4 +1,15 @@
 package adelina.luxtravel.service;
 
+import adelina.luxtravel.repository.BookingDataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookingDataService {
+    private BookingDataRepository bookingDataRepository;
+
+    @Autowired
+    public BookingDataService(BookingDataRepository bookingDataRepository) {
+        this.bookingDataRepository = bookingDataRepository;
+    }
 }
